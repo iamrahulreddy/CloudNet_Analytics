52d87446a66147423f220d4304779cd4184884d9
Muskula Rahul <iamrahul250@gmail.com>
2025-03-31 21:32:57 +0530

    Update orchestrate.sh

--- processor-server/orchestrate.sh
+++ processor-server/orchestrate.sh
@@ -5,7 +5,7 @@ ZONE="us-central1-a"
 PROJECT="<Your_GCP_PROJECT>"
 
 for vm in "${VMS[@]}"; do
-    gcloud compute scp "iamrahul251@$vm:<PATH>/logs/*.log" /tmp/ --zone="$ZONE" --project="$PROJECT" || { echo "SCP failed for $vm"; exit 1; }
+    gcloud compute scp "<User_Name>@$vm:<PATH>/logs/*.log" /tmp/ --zone="$ZONE" --project="$PROJECT" || { echo "SCP failed for $vm"; exit 1; }
 done
 
 python3 <PATH>/process_logs.py || { echo "Processing failed"; exit 1; }
